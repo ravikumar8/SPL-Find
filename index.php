e343d82297b0af361fc327c0f4431dcb3d1d121a
Ravi Kumar <idea.begins.here@gmail.com>
2014-08-07 14:29:40 +0530

    removed profiler

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
 
-include_once 'Profiler.php';
 include_once 'Find.php';
 
 $path 		=	realpath( 'D:\xampp\htdocs\webloper\Gallery\uploads' );
@@ -11,9 +10,3 @@ $files = $find->in( $path );
 foreach( $files as $key => $file) {
 	echo '<pre> ' , $key . ' <br> ' , print_r($file, 1), '</pre>';
 }
-
-$profiler = new Profiler;
-
-echo '<pre>',print_r( Profiler::included_files(), 1 );
-
-echo '<pre>',print_r( $profiler->printMemoryUsageInformation(), 1 );
